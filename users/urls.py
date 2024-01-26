8e9bbbccaf2f824a7f4c0a214e4e9616b8670ba7
smintank <assbestmail@gmail.com>
2024-01-26 13:55:21 +0300

    Add users urls

---
+++ users/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+
+from users.views import (
+    ProfileView,
+    ProfileEditView,
+    ChangePasswordView,
+    ChangeEmailView,
+    ChangePhoneNumberView
+)
+
+app_name = "users"
+
+urlpatterns = [
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("profile/edit/", ProfileEditView.as_view(), name="profile_edit"),
+    path(
+        "profile/change-password/", ChangePasswordView.as_view(), name="change_password"
+    ),
+    path("profile/change-email/", ChangeEmailView.as_view(), name="change_email"),
+    path(
+        "profile/change-phone/",
+        ChangePhoneNumberView.as_view(),
+        name="change_phone_number",
+    ),
+]
