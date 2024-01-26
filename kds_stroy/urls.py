98708a0bc842f5e65714dc54d0a25bcc7974746f
smintank <assbestmail@gmail.com>
2024-01-26 13:45:38 +0300

    Add urls in main urls file

--- kds_stroy/urls.py
+++ kds_stroy/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for kds_stroy project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+from kds_stroy import settings
+from users.views import register
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("admin/", admin.site.urls),
+    path("auth/", include("django.contrib.auth.urls")),
+    path("auth/registration/", register, name="registration"),
+    path("users/", include("users.urls")),
+    path("orders/", include("orders.urls")),
+    path("news/", include("news.urls")),
+    path(
+        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
+    ),
 ]
