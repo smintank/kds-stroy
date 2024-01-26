51aa3d7f6b7767e953da880f8e28524150ca792e
smintank <assbestmail@gmail.com>
2024-01-26 13:40:52 +0300

    Add User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    middle_name = models.CharField("Отчество", max_length=50, blank=True)
+    avatar = models.ImageField("Фото профиля", upload_to="avatars/",
+                               default="avatars/default.png")
+    email = models.EmailField("Электронная почта", unique=True)
+    is_email_verified = models.BooleanField(default=False)
+    phone_number = PhoneNumberField("Номер телефона", unique=True)
+    is_phone_verified = models.BooleanField(default=False)
+    address = models.ForeignKey(
+        "orders.Address",
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Адрес",
+    )
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "Пользователи"
