d26c36fff9f708fb9032f2e172eebc87f2d0d49c
smintank <assbestmail@gmail.com>
2024-02-01 16:13:52 +0300

    Add auto-generated username in models with UUID

--- users/models.py
+++ users/models.py
@@ -1,9 +1,11 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-
+    first_name = models.CharField("Имя", max_length=150, blank=False, null=False, help_text="Обязательное поле")
     middle_name = models.CharField("Отчество", max_length=150, blank=True)
     avatar = models.ImageField("Фото профиля", upload_to="avatars/",
                                default="avatars/default.png")
@@ -22,7 +24,17 @@ class User(AbstractUser):
     last_login = models.DateTimeField("Последний вход", auto_now=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name"]
+    REQUIRED_FIELDS = ["first_name", "username", "phone_number"]
+
+    def save(self, *args, **kwargs):
+        is_unique = False
+        while not is_unique:
+            unique_username = str(uuid.uuid1())[:10]
+            unique_username = f"User-{unique_username}"
+            if not User.objects.filter(username=unique_username).exists():
+                is_unique = True
+                self.username = unique_username
+                super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.email}"
