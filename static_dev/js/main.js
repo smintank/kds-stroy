34f0959dfaa1927570dff0ad19614b6e634fd519
smintank <assbestmail@gmail.com>
2024-02-18 15:08:58 +0300

    Add blocking of submit button if checkbox is unchecked

--- static_dev/js/main.js
+++ static_dev/js/main.js
@@ -1,18 +1,25 @@
 document.addEventListener("DOMContentLoaded",function() {
     let header = $('.header');
-    let headerHeight = header.height(); // вычисляем высоту шапки
+    let headerHeight = header.height();
      
     $(window).scroll(function() {
         if($(this).scrollTop() > 1) {
             header.addClass('header_fixed');
-            $('body').css({
-                'paddingTop': headerHeight+'px' // делаем отступ у body, равный высоте шапки
-            });
+            $('body').css({'paddingTop': headerHeight+'px'});
         } else {
             header.removeClass('header_fixed');
-            $('body').css({
-                'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
-            })
+            $('body').css({'paddingTop': 0})
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  var checkbox = document.getElementById("checkbox");
+  var button = document.getElementById("order_button");
+
+  checkbox.addEventListener("change", function() {
+    button.disabled = !this.checked;
+    button.style.backgroundColor = this.checked ? "#C82027" : "#c89fa1";
+  });
+});
+
