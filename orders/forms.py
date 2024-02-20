3b238498c57731fa41bea44b69b9100ffea7f18e
smintank <assbestmail@gmail.com>
2024-02-21 00:01:02 +0300

    Change order form for multiple files uploading

--- orders/forms.py
+++ orders/forms.py
@@ -1,14 +1,33 @@
 import re
-
 from django import forms
 
 from .models import Order
 
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+
 class OrderCreationForm(forms.ModelForm):
+    photo = MultipleFileField(label='Фото', required=False)
+
     class Meta:
         model = Order
-        fields = ('first_name', 'phone_number', 'address', 'comment', 'photo')
+        fields = ['first_name', 'phone_number', 'address', 'comment', 'photo']
         widgets = {
             'phone_number': forms.TextInput(
                 attrs={'type': "tel", 'data-tel-input': "",
