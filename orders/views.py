388b7b86d5cc5dead258b2edf863b9e7be3a8c81
smintank <assbestmail@gmail.com>
2024-02-20 10:13:49 +0300

    Change order view method. If post, return jsonResponse Add file transmitting

--- orders/views.py
+++ orders/views.py
@@ -5,14 +5,11 @@ from .forms import OrderCreationForm
 
 def order(request):
     if request.method == "POST":
-        order_form = OrderCreationForm(request.POST)
+        order_form = OrderCreationForm(request.POST, request.FILES)
         if order_form.is_valid():
             order_form.save()
-            return render(
-                request,
-                'pages/main.html',
-                {"order": order_form}
-            )
+            return JsonResponse({'message': 'Form submitted successfully'},
+                                status=201)
     else:
         order_form = OrderCreationForm()
     return render(
