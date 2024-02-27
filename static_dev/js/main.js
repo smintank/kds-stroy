453559dc1741ac9ce7a2f9b66d5f26ece2899d31
smintank <assbestmail@gmail.com>
2024-02-27 14:00:14 +0300

    Add cookies allowing banner

--- static_dev/js/main.js
+++ static_dev/js/main.js
@@ -25,8 +25,42 @@ $(document).ready(function() {
 });
 
 
-    checkbox.addEventListener("change", function() {
-        button.disabled = !this.checked;
-        button.style.backgroundColor = this.checked ? "#C82027" : "#c89fa1";
+$(document).ready(function() {
+    const cookieBanner = $('#cookieBanner');
+
+    // Check if the user has accepted cookies
+    const acceptedCookies = getCookie('acceptedCookies');
+    if (!acceptedCookies) {
+        cookieBanner.show();
+    }
+
+    // When the user clicks on the accept button
+    $('#acceptCookies').click(function() {
+        // Set a cookie to indicate that the user has accepted cookies
+        setCookie('acceptedCookies', true, 365); // Expires in 365 days
+        cookieBanner.hide();
     });
+
+    // Function to set a cookie
+    function setCookie(name, value, days) {
+        let expires = "";
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+
+    // Function to get a cookie
+    function getCookie(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
 });
