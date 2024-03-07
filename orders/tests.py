e3d7421fdd76db7682ad4c4d86511f93d677c7ae
smintank <assbestmail@gmail.com>
2024-03-07 11:03:33 +0300

    Add simple order tests

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from orders.models import Order, OrderPhoto
 
-# Create your tests here.
+
+class OrderModelTest(TestCase):
+    def setUp(self):
+        self.order = Order.objects.create(
+            first_name='John',
+            phone_number='1234567890',
+            comment='test comment')
+
+    def test_order_creation(self):
+        self.assertEqual(f'{self.order.first_name}', 'John')
+        self.assertEqual(f'{self.order.phone_number}', '1234567890')
+        self.assertEqual(f'{self.order.comment}', 'test comment')
+        self.assertEqual(f'{self.order.status}', 'Зарегистрирован')
+
+
+class OrderPhotoModelTest(TestCase):
+    def setUp(self):
+        self.order = Order.objects.create(
+            first_name='John',
+            phone_number='1234567890',
+            comment='test comment')
+        self.photo = OrderPhoto.objects.create(
+            order=self.order,
+            photo='test.jpg'
+        )
+
+    def test_order_photo_creation(self):
+        self.assertEqual(f'{self.photo.photo}', 'test.jpg')
