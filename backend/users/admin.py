fe749838e947a50b7a67153b3da13de547f9f91f
smintank <assbestmail@gmail.com>
2024-04-02 19:31:43 +0300

    Setup admin panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from orders.models import Order
 from django.contrib.auth.models import Group
+from verify_email.admin import LinkCounter
+
+
+class OrderAdmin(admin.TabularInline):
+    model = Order
+    extra = 0
 
 
 class MyUserAdmin(UserAdmin):
@@ -17,6 +24,7 @@ class MyUserAdmin(UserAdmin):
     list_display_links = ("email", "phone_number")
     readonly_fields = ("last_login", "date_joined", "email")
     ordering = ["id"]
+    inlines = [OrderAdmin]
 
 
 admin.site.register(User, MyUserAdmin)
@@ -26,3 +34,4 @@ admin.site.site_title = "КДС-Строй"
 admin.site.index_title = "Панель администратора"
 admin.site.empty_value_display = "Не задано"
 admin.site.unregister(Group)
+admin.site.unregister(LinkCounter)
