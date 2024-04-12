c202d257d45bc4f9779e4a009caf8da6f6f84ac6
smintank <assbestmail@gmail.com>
2024-04-12 17:40:47 +0300

    Main js refactoring
    - Add message popup class

--- backend/static/js/main-WCi7f0ZE.js
+++ backend/static/js/main-WCi7f0ZE.js
@@ -1,16 +1,22 @@
 import "./modulepreload-polyfill-BoyGcPDr.js";
-import { P as Popup, u as useBurger, a as useHeaderOnScroll, PP as PromotionPopup } from "./headerOnScroll-BsQcXGBC.js";
+import { P as Popup, MP as MessagePopup, u as useBurger, a as useHeaderOnScroll,
+  PP as PromotionPopup, s as setCookie, g as getCookie, c as useCookieBanner,
+  n as useActiveNavSection
+} from "./headerOnScroll-BsQcXGBC.js";
 import { u as useShowcaseModal } from "./showcaseModal-BGg2r-Tz.js";
 import { u as useProjectsRepairsSlider, a as useProjectsBigsSlider, b as useReviewsSlider } from "./sliders-C0i03PCH.js";
 import { u as useInputPhoneMask } from "./useInputPhoneMask-CE77khD0.js";
 
 const popupOrder = new Popup("#popup-order");
-popupOrder.setEventListeners();
+const popupNewsSub = new MessagePopup("#popup-news-sub");
+const popupMessage = new MessagePopup("#popup-message");
+const promotionPopup = new PromotionPopup("#popup-promotion");
 
-const popupMessage = new Popup("#popup-message");
+popupOrder.setEventListeners();
+popupNewsSub.setEventListeners();
+promotionPopup.setEventListeners();
 popupMessage.setEventListeners();
-const messageElement = document.querySelector("#messageTitle");
-const textElement = document.querySelector("#messageText");
+
 let cityChosen = false;
 
 
@@ -32,34 +38,82 @@ const useAccordion = () => {
   });
 };
 
-const useActiveNavSection = () => {
-  const navLinks = document.querySelectorAll(".header__nav-list-item a");
+const useCustomAnchorScroll = () => {
   const headerHeight = document.querySelector(".header").offsetHeight;
-  window.onscroll = function() {
-    navLinks.forEach((navLink) => {
-      if (navLink.getAttribute("href") === "#")
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+      const element = document.querySelector(this.getAttribute("href"));
+      if (!element)
         return;
-      const section = document.querySelector(navLink.getAttribute("href"));
-      if (section.offsetTop - headerHeight <= window.scrollY && section.offsetTop + section.offsetHeight > window.scrollY) {
-        navLink.classList.add("header__nav-list-item--active");
-      } else {
-        navLink.classList.remove("header__nav-list-item--active");
-      }
+      window.scrollTo(0, element.offsetTop - headerHeight);
     });
-  };
+  });
+};
+
+const useWorksToggle = () => {
+  const links = document.querySelectorAll(".hero__footer-list-item-link");
+  const works = document.querySelectorAll(".works__list-item");
+  links.forEach((elem) => {
+    elem.addEventListener("click", (e2) => {
+      links.forEach((elem2) => {
+        elem2.parentNode.classList.remove("hero__footer-list-item--active");
+      });
+      elem.parentNode.classList.add("hero__footer-list-item--active");
+      let activeWork;
+      works.forEach((elem2) => {
+        elem2.classList.remove("works__list-item--active");
+        if (`${elem2.id}-link` === e2.target.id) {
+          activeWork = elem2;
+        }
+      });
+      activeWork.classList.add("works__list-item--active");
+    });
+  });
+};
+
+const useEmailSubscription = () => {
+  const emailSubscribed = getCookie('email_subscribed');
+    if (emailSubscribed) {
+        document.getElementById('stocksFormContainer').style.display = 'none';
+    }
+
+  const emailForm = document.querySelector("#formStocks");
+  emailForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const stocksFormContainer = document.querySelector("#stocksFormContainer");
+    fetch("/subs/subscribe/", {
+      method: "POST",
+      body: new FormData(e.target),
+    }).then(function(response) {
+      if (response.ok) {
+        stocksFormContainer.innerHTML = '' +
+          '<h2 class="popup__title">Мы рады, что вы с нами!</h2>\n' +
+          '<p class="popup__text">\n' +
+          'Теперь вы всегда будете в курсе наших новостей, акций и специальных предложений.\n' +
+          '</p>';
+        setCookie('email_subscribed', true, 1);
+      } else {
+        throw new Error(response.statusText);
+      }
+    }).catch(function(error) {
+      popupMessage.open('Ошибка при создании заказа!', 'Не удалось подписаться, попробуйте позже!');
+    });
+  });
+};
+
+const usePromotionPopup = () => {
+    if (getCookie("promo_popup_hide") === 'true') {
+    promotionPopup.close();
+  } else {
+    promotionPopup.open();
+  }
 };
 
 
-const useContactsFormWithImages = () => {
+const useOrderFormWithImages = () => {
   const formData = new FormData();
   let imageNumbers = 0;
-
-  function isPhotoAlreadyAdded(formData2, photoName) {
-    // Check if the file is already in the form data
-    return Array.from(formData2.values()).some(
-      (photo) => photo.name === photoName
-    );
-  }
 
   function togglePhotoTools(formData2) {
     const formDataLength = Array.from(formData2.entries()).length;
@@ -95,7 +149,6 @@ const useContactsFormWithImages = () => {
 
   function handleFileInputChange(e) {
     const photos = e.target.files;
-    console.log("Files:", photos); // Log the files obtained from the input event
 
     Array.from(photos).forEach((photo) => {
       if (Array.from(formData.entries()).length < 5) {
@@ -121,12 +174,7 @@ const useContactsFormWithImages = () => {
   document.getElementById("orderForm").addEventListener("submit", function(e) {
     e.preventDefault();
     popupOrder.close();
-    popupMessage.open();
-
-    if (messageElement) {
-      messageElement.textContent = 'Ожидайте...';
-      textElement.textContent = 'Ваша заявка обрабатывается';
-    }
+    popupMessage.open('Ожидайте...', 'Ваша заявка обрабатывается');
 
     const originFormData = new FormData(this);
     originFormData.delete("photo");
@@ -136,6 +184,7 @@ const useContactsFormWithImages = () => {
     if (!cityChosen) {
        originFormData.delete("city");
     }
+
     this.reset();
     fetch("/orders/create/", {
       method: "POST",
@@ -147,177 +196,27 @@ const useContactsFormWithImages = () => {
         throw new Error(response.statusText);
       }
     }).then(function(data) {
-        if (messageElement) {
-          setCookie('order_created', true, 1);
-          messageElement.textContent = data.message;
-          textElement.textContent = data.text
-        }
+      setCookie('order_created', true, 1);
+      popupMessage.update(data.message, data.text);
     }).catch(function(error) {
       console.log(error);
-      if (messageElement) {
-          messageElement.textContent = 'Ошибка при создании заказа!';
-          textElement.textContent = 'Попробуйте позже или свяжитесь с нами';
-        }
-    });
-  });
-};
-
-
-function setCookie(cname, cvalue, exdays) {
-  const d = /* @__PURE__ */ new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1e3);
-  const expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-function getCookie(cname) {
-  const name = cname + "=";
-  const ca = document.cookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) === " ")
-      c = c.substring(1);
-    if (c.indexOf(name) === 0)
-      return c.substring(name.length, c.length);
-  }
-  return false;
-}
-
-const useCustomAnchorScroll = () => {
-  const headerHeight = document.querySelector(".header").offsetHeight;
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function(e) {
-      e.preventDefault();
-      const element = document.querySelector(this.getAttribute("href"));
-      if (!element)
-        return;
-      window.scrollTo(0, element.offsetTop - headerHeight);
-    });
-  });
-};
-
-const useWorksToggle = (e) => {
-  const links = document.querySelectorAll(".hero__footer-list-item-link");
-  const works = document.querySelectorAll(".works__list-item");
-  links.forEach((elem) => {
-    elem.addEventListener("click", (e2) => {
-      links.forEach((elem2) => {
-        elem2.parentNode.classList.remove("hero__footer-list-item--active");
-      });
-      elem.parentNode.classList.add("hero__footer-list-item--active");
-      let activeWork;
-      works.forEach((elem2) => {
-        elem2.classList.remove("works__list-item--active");
-        if (`${elem2.id}-link` === e2.target.id) {
-          activeWork = elem2;
-        }
-      });
-      activeWork.classList.add("works__list-item--active");
-    });
-  });
-};
-
-addEventListener("DOMContentLoaded", () => {
-  useBurger();
-  useHeaderOnScroll();
-  useProjectsRepairsSlider();
-  useProjectsBigsSlider();
-  useReviewsSlider();
-  useAccordion();
-  useInputPhoneMask();
-  useWorksToggle();
-  useShowcaseModal();
-  useContactsFormWithImages();
-  useCustomAnchorScroll();
-  useActiveNavSection();
-
-  const popupNewsSub = new Popup("#popup-news-sub");
-  popupNewsSub.setEventListeners();
-
-  const promotionPopup = new PromotionPopup("#popup-promotion");
-  promotionPopup.setEventListeners();
-
-  if (getCookie("promo_popup_hide") === 'true') {
-    promotionPopup.close();
-  } else {
-    promotionPopup.open();
-  }
-
-
-  const formStocks = document.querySelector("#formStocks");
-  formStocks.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const stocksFormContainer = document.querySelector("#stocksFormContainer");
-    fetch("/subs/subscribe/", {
-      method: "POST",
-      body: new FormData(e.target),
-    }).then(function(response) {
-      if (response.ok) {
-        stocksFormContainer.innerHTML = '' +
-          '<h2 class="popup__title">Мы рады, что вы с нами!</h2>\n' +
-          '<p class="popup__text">\n' +
-          'Теперь вы всегда будете в курсе наших новостей, акций и специальных предложений.\n' +
-          '</p>';
-        setCookie('email_subscribed', true, 1);
-      } else {
-        throw new Error(response.statusText);
-      }
-    }).catch(function(error) {
-      console.log(error);
-      messageElement.textContent = 'Ошибка при создании заказа!';
-      textElement.textContent = 'Не удалось подписаться, попробуйте позже!';
-      popupMessage.open();
+      popupMessage.update('Ошибка при создании заказа!', 'Попробуйте позже или свяжитесь с нами');
     });
   });
 
-  const orderPopupButton = document.querySelectorAll("#orderPopup");
-  orderPopupButton.forEach((button) => {
+  document.querySelectorAll("#orderPopup").forEach((button) => {
     button.addEventListener("click", () => {
       if (getCookie("order_created") !== "true") {
         popupOrder.open();
       } else {
-        messageElement.textContent = "Вы уже отправили заявку!";
-        textElement.textContent = "Мы свяжемся с вами в ближайшее время";
-        popupMessage.open();
+        popupMessage.open("Вы уже отправили заявку!", "Мы свяжемся с вами в ближайшее время");
       }
     });
   });
 
-  const emailSubscribed = getCookie('email_subscribed');
-    if (emailSubscribed) {
-        document.getElementById('stocksFormContainer').style.display = 'none';
-    }
+};
 
-  const cookieBanner = document.getElementById('cookieBanner');
-  const acceptedCookies = getCookie('acceptedCookies');
-  if (!acceptedCookies) cookieBanner.style.display = 'flex';
-
-  document.getElementById('acceptCookies').addEventListener("click", function() {
-      setCookie('acceptedCookies', true, 365); // Expires in 365 days
-      cookieBanner.style.display = 'none';
-  });
-
-
-  // Auth and Sign In Popups
-  // const popupSuccess2 = new Popup("#popup-success");
-  // popupSuccess2.setEventListeners();
-  // const popupFailure2 = new Popup("#popup-failure");
-  // popupFailure2.setEventListeners();
-  // const popupAuth = new Popup("#popup-auth");
-  // if (popupAuth) {
-  //   popupAuth.setEventListeners();
-  // }
-  // const signInButton = document.querySelector("#signIn");
-  // if (signInButton) {
-  //   signInButton.addEventListener("click", () => popupAuth.open());
-  // }
-  // const formAuth = document.querySelector("#formAuth");
-  // formAuth.addEventListener("submit", (e) => {
-  //   e.preventDefault();
-  //   popupAuth.close();
-  //   formAuth.reset();
-  //   popupSuccess2.open();
-  // });
-
+const useCitySuggestions = () => {
   const cityField = document.getElementById("id_city");
   let dropdown;
 
@@ -384,6 +283,43 @@ addEventListener("DOMContentLoaded", () => {
   });
 
   window.addEventListener('resize', updateDropdownPosition);
+};
+
+const useAuthPopup = () => {
+  const popupAuth = new Popup("#popup-auth");
+  popupAuth.setEventListeners();
+
+  const signInButton = document.querySelector("#signIn");
+  signInButton.addEventListener("click", () => popupAuth.open());
+
+  const formAuth = document.querySelector("#formAuth");
+  formAuth.addEventListener("submit", (e) => {
+    e.preventDefault();
+    popupAuth.close();
+    formAuth.reset();
+    popupMessage.open('Вход выполнен!', 'Добро пожаловать!');
+  });
+}
+
+
+addEventListener("DOMContentLoaded", () => {
+  useBurger();
+  useHeaderOnScroll();
+  useProjectsRepairsSlider();
+  useProjectsBigsSlider();
+  useReviewsSlider();
+  useAccordion();
+  useInputPhoneMask();
+  useWorksToggle();
+  useShowcaseModal();
+  useOrderFormWithImages();
+  useCustomAnchorScroll();
+  useActiveNavSection();
+  useCookieBanner();
+  useCitySuggestions();
+  useEmailSubscription();
+  usePromotionPopup();
+  // useAuthPopup();
 });
 
 
--- backend/static/js/terms.js
+++ backend/static/js/terms.js
@@ -1,44 +1,13 @@
 import "./modulepreload-polyfill-BoyGcPDr.js";
-import { u as useBurger, a as useHeaderOnScroll } from "./headerOnScroll-BsQcXGBC.js";
+import { u as useBurger, a as useHeaderOnScroll, c as useCookieBanner } from "./headerOnScroll-BsQcXGBC.js";
 import { u as useShowcaseModal } from "./showcaseModal-BGg2r-Tz.js";
 import { u as useInputPhoneMask } from "./useInputPhoneMask-CE77khD0.js";
 
-
-function setCookie(cname, cvalue, exdays) {
-  const d = /* @__PURE__ */ new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1e3);
-  const expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-function getCookie(cname) {
-  const name = cname + "=";
-  const ca = document.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ")
-      c = c.substring(1);
-    if (c.indexOf(name) == 0)
-      return c.substring(name.length, c.length);
-  }
-  return false;
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Function to show the cookie banner if the user has not accepted cookies
-    const cookieBanner = document.getElementById('cookieBanner');
-    const acceptedCookies = getCookie('acceptedCookies');
-    if (!acceptedCookies) cookieBanner.style.display = 'flex';
-
-    document.getElementById('acceptCookies').addEventListener("click", function() {
-        setCookie('acceptedCookies', true, 365); // Expires in 365 days
-        cookieBanner.style.display = 'none';
-    });
-});
 
 addEventListener("DOMContentLoaded", () => {
   useBurger();
   useHeaderOnScroll();
   useInputPhoneMask();
   useShowcaseModal();
-
+  useCookieBanner();
 });
