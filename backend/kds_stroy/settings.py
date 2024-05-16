25422c482a258e2e24b30ebb67802964c173ac7d
smintank <assbestmail@gmail.com>
2024-05-16 11:32:31 +0400

    Fix flake8 tests

--- backend/kds_stroy/settings.py
+++ backend/kds_stroy/settings.py
@@ -168,7 +168,7 @@ LOGIN_URL = "login"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = bool(int(os.getenv("EMAIL_TLS", default=0)))
 EMAIL_USE_SSL = bool(int(os.getenv("EMAIL_SSL", default=0)))
 EMAIL_HOST_USER = os.environ.get("EMAIL_ID")
