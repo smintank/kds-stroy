54ebdb6b5d69bc0ba05f23fa3330b42eed89548d
smintank <assbestmail@gmail.com>
2024-05-21 15:26:45 +0400

    Fix wrong single news showing

--- backend/news/views.py
+++ backend/news/views.py
@@ -8,7 +8,7 @@ class NewsListView(ListView):
     context_object_name = "news"
     queryset = News.published.all()
     paginate_by = 10
-    ordering = "-created_at"
+    ordering = "-published_date"
     model = News
 
     def get_context_data(self, *, object_list=None, **kwargs):
