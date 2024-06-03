e2b9590a70817097c144af1e9fab3f06f502a091
smintank <assbestmail@gmail.com>
2024-06-03 16:41:57 +0400

    Pep8 fixes

--- backend/kds_stroy/urls.py
+++ backend/kds_stroy/urls.py
@@ -29,7 +29,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
 
 handler404 = handler404
