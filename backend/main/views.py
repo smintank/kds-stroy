8a6ef89f5e78753081abf339ea18d126e8e36ba8
smintank <assbestmail@gmail.com>
2024-06-03 16:29:46 +0400

    Create mixin for adding ordering context

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,42 +1,18 @@
-# from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from ads_mailing.forms import SubscribeForm
-from orders.forms import OrderCreationForm
-from orders.models import Order
+from orders.views import OrderContextMixin
 
 
 def handler404(request, exception):
     return render(request, "404.html", status=404)
 
 
-class MainView(TemplateView):
+class MainView(OrderContextMixin, TemplateView):
     template_name = "pages/main.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["order_form"] = OrderCreationForm()
         context["subscribe_form"] = SubscribeForm()
-        context["view_name"] = self.__class__.__name__
-
-        order_id = self.request.session.get("order_id")
-        order = Order.objects.filter(order_id=order_id).first()
-        if order and order.status in [Order.Status.COMPLETED,
-                                      Order.Status.CANCELED]:
-            self.request.session["order_id"] = None
-            self.request.session["order_created"] = None
-
-        # login_form = AuthenticationForm()
-        # context["login_form"] = login_form
         return context
-
-    def render_to_response(self, context, **response_kwargs):
-        response = super(MainView, self).render_to_response(context,
-                                                            **response_kwargs)
-
-        order_id = self.request.session.get("order_id")
-        order_created = 1 if self.request.session.get("order_created") else 0
-        response.set_cookie('order_id', order_id, max_age=360000)
-        response.set_cookie('order_created', order_created, max_age=360000)
-        return response
--- backend/news/views.py
+++ backend/news/views.py
@@ -1,55 +1,35 @@
 from django.views.generic import ListView, DetailView
 
 from news.models import News, Category
-from orders.forms import OrderCreationForm
-from orders.models import Order
+from orders.views import OrderContextMixin
 
 
-class NewsListView(ListView):
+class NewsListView(OrderContextMixin, ListView):
+    model = News
     template_name = "pages/news_list.html"
-    context_object_name = "news"
-    queryset = News.published.all()
     paginate_by = 10
     ordering = "-published_date"
-    model = News
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if category := self.request.GET.get("category"):
+            queryset = queryset.filter(category__slug=category.lower())
+
+        order_by = self.request.GET.get("sorting", "newest")
+        if order_by == "oldest":
+            queryset = queryset.order_by("published_date")
+        else:
+            queryset = queryset.order_by("-published_date")
+
+        return queryset
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context["categories"] = Category.objects.all()
-        context["order_form"] = OrderCreationForm()
-        context["view_name"] = self.__class__.__name__
-
-        order_id = self.request.session.get("order_id")
-        order = Order.objects.filter(order_id=order_id).first()
-        if order and order.status in [Order.Status.COMPLETED,
-                                      Order.Status.CANCELED]:
-            self.request.session["order_id"] = None
-            self.request.session["order_created"] = None
-
-        # login_form = AuthenticationForm()
-        # context["login_form"] = login_form
         return context
 
 
-class NewsDetailView(DetailView):
+class NewsDetailView(OrderContextMixin, DetailView):
     model = News
-    slug_field = "pk"
-    slug_url_kwarg = "pk"
     template_name = "pages/news_detail.html"
-    context_object_name = "news"
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["order_form"] = OrderCreationForm()
-        context["view_name"] = self.__class__.__name__
-
-        order_id = self.request.session.get("order_id")
-        order = Order.objects.filter(order_id=order_id).first()
-        if order and order.status in [Order.Status.COMPLETED,
-                                      Order.Status.CANCELED]:
-            self.request.session["order_id"] = None
-            self.request.session["order_created"] = None
-
-        # login_form = AuthenticationForm()
-        # context["login_form"] = login_form
-        return context
