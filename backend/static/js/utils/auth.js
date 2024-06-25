2552e1257d302c2624d3a1d1d67577c9bb47f997
smintank <assbestmail@gmail.com>
2024-06-26 00:27:48 +0400

    Add login and logout popups and buttons to the main page

--- backend/static/js/utils/auth.js
+++ backend/static/js/utils/auth.js
@@ -1,19 +1,22 @@
-import { P as Popup, MP as popupMessage } from "./popups.js";
+import { P as Popup} from "./popups.js";
 
 const useAuthPopup = () => {
-    const popupAuth = new Popup("#popup-auth");
-    popupAuth.setEventListeners();
+    if (document.querySelector("#popup-auth") !== null) {
+        const popupAuth = new Popup("#popup-auth");
+        popupAuth.setEventListeners();
 
-    const signInButton = document.querySelector("#signIn");
-    signInButton.addEventListener("click", () => popupAuth.open());
+        const signInButton = document.querySelector("#signIn");
+        signInButton.addEventListener("click", () => popupAuth.open());
+    } else if (document.querySelector("#popup-logout") !== null) {
+        const popupLogout = new Popup("#popup-logout");
+        popupLogout.setEventListeners();
 
-    const formAuth = document.querySelector("#formAuth");
-    formAuth.addEventListener("submit", (e) => {
-        e.preventDefault();
-        popupAuth.close();
-        formAuth.reset();
-        popupMessage.open('Вход выполнен!', 'Добро пожаловать!');
-    });
+        const logoutButton = document.querySelector("#show_logout_popup");
+        logoutButton.addEventListener("click", () => popupLogout.open());
+
+        const logoutCancelButton = document.querySelector("#logoutCancelButton");
+        logoutCancelButton.addEventListener("click", () => popupLogout.close());
+    }
 };
 
 export { useAuthPopup as Au };
