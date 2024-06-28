fb021897ad8e8af448040c1f20c6d3a9175b9d37
smintank <assbestmail@gmail.com>
2024-06-28 17:29:35 +0400

    Add django-widget-tweaks for templates

--- backend/static/js/pages/account-page.js
+++ backend/static/js/pages/account-page.js
@@ -2,10 +2,11 @@ import "../utils/modulepreload-polyfill.js";
 import { u as useBurger, a as useHeaderOnScroll} from "../utils/base.js";
 import { u as useShowcaseModal } from "../utils/showcase-modal.js";
 import { u as useInputPhoneMask } from "../utils/input-phone-mask.js";
+import { uOP as useOrderPopup} from "../utils/orders.js";
+import { Au as useAuthPopup } from "../utils/auth.js";
 
 const useAccountItem = () => {
   const accountItems = document.querySelectorAll(".applications__list-item");
-  console.log(accountItems);
   accountItems.forEach((item) => {
     item.addEventListener("click", (e) => {
       if (item.classList.contains("applications__list-item--open")) {
@@ -22,7 +23,9 @@ const useAccountItem = () => {
 addEventListener("DOMContentLoaded", () => {
   useBurger();
   useHeaderOnScroll();
+  useOrderPopup();
   useInputPhoneMask();
   useAccountItem();
   useShowcaseModal();
+  useAuthPopup();
 });
