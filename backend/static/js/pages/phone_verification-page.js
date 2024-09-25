e7f85e9a41de2cebec8e0b44600e3bdeb8b94609
smintank <assbestmail@gmail.com>
2024-09-25 22:29:00 +0300

    Finish phone verification frontend logic and style

--- backend/static/js/pages/phone_verification-page.js
+++ backend/static/js/pages/phone_verification-page.js
@@ -10,49 +10,47 @@ import { uPP as usePromotionPopup } from "../utils/popups.js";
 import { Au as useAuthPopup } from "../utils/auth.js";
 import { SI as useSmsInput } from "../utils/smscode_input.js";
 
-function useSMSVerificationTimer() {
-  document.addEventListener("DOMContentLoaded", function () {
-    const countdownElement = document.getElementById('countdown');
-    const linkContainer = document.getElementById('linkContainer');
-    let countdownDuration = parseInt(countdownElement.dataset.countdownDuration);
-    let timerInterval;
+const useSMSVerificationTimer = () => {
+  const countdownElement = document.getElementById('countdown');
+  const countdownBlock = document.getElementById('countdownBlock');
+  const repeatButton = document.getElementById("repeatCallButton");
+  let countdownDuration = parseInt(countdownElement.dataset.countdownDuration);
+  let timerInterval;
 
-    function startTimer() {
-      let minutes, seconds;
-      timerInterval = setInterval(function () {
-        minutes = Math.floor(countdownDuration / 60);
-        seconds = countdownDuration % 60;
+  function startTimer() {
+    let minutes, seconds;
+    timerInterval = setInterval(function () {
+      minutes = Math.floor(countdownDuration / 60);
+      seconds = countdownDuration % 60;
 
-        countdownElement.textContent = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+      countdownElement.textContent = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
 
-        if (--countdownDuration < 0) {
-          clearInterval(timerInterval);
-          linkContainer.style.display = 'block';
-          countdownElement.style.display = 'none';
-        }
-      }, 1000);
+      if (--countdownDuration < 0) {
+        clearInterval(timerInterval);
+        repeatButton.style.display = 'block';
+        countdownBlock.style.display = 'none';
+      }
+    }, 1000);
+    if (countdownDuration > 0) {
+      repeatButton.style.display = "none";
     }
+  }
 
-    document.getElementById("repeatCallButton").addEventListener("click", function () {
-      let data = {
-        repeat_call: true
-      };
-      fetch("/profile/phone_verification/?repeat_call=true")
-        .then(response => response.json())
-        .then(data => {
-          countdownDuration = data.countdown;
-          countdownElement.style.display = 'block';
-          linkContainer.style.display = 'none';
-          startTimer();
-          console.log('Повторный звонок');
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    });
-
-    startTimer();
+  document.getElementById("repeatCallButton").addEventListener("click", function () {
+    fetch("/profile/phone_verification/?repeat_call=true")
+      .then(response => response.json())
+      .then(data => {
+        countdownDuration = data.countdown;
+        countdownBlock.style.display = 'block';
+        repeatButton.style.display = 'none';
+        startTimer();
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   });
+
+  startTimer();
 }
 
 addEventListener("DOMContentLoaded", () => {
