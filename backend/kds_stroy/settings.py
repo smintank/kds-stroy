d68f87c16080c159e924b0c8ca751972337e979e
smintank <assbestmail@gmail.com>
2024-10-20 22:59:30 +0400

    Fix settings and dockerfile

--- backend/kds_stroy/settings.py
+++ backend/kds_stroy/settings.py
@@ -88,7 +88,7 @@ LOGGING = {
             "class": "logging.StreamHandler",
         },
         "file": {
-            "level": "DEBUG",
+            "level": "ERROR",
             "class": "logging.FileHandler",
             "filename": "error.log",
         },
@@ -96,7 +96,7 @@ LOGGING = {
     "loggers": {
         "django": {
             "handlers": ["console", "file"],
-            "level": "DEBUG",
+            "level": "INFO",
             "propagate": True,
         },
     },
