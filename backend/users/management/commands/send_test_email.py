164116f533ffa76760eafc172dcd7ffe04dd21ee
smintank <assbestmail@gmail.com>
2025-01-20 00:03:37 +0400

    Add django commands for email and tg send testing

---
+++ backend/users/management/commands/send_test_email.py
@@ -0,0 +1,31 @@
+from django.core.mail import send_mail
+from django.core.management.base import BaseCommand
+
+from kds_stroy.settings import DEFAULT_FROM_EMAIL
+
+
+# python manage.py send_test_email name@domain.com
+
+class Command(BaseCommand):
+    help = 'Отправить тестовое письмо'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--to',
+            type=str,
+            help='Email получателя',
+            required=True,
+        )
+
+    def handle(self, *args, **options):
+        recipient = options['to']
+        try:
+            send_mail(
+                subject='Тестовое письмо',
+                message='Это тестовое письмо, отправленное из Django.',
+                from_email=DEFAULT_FROM_EMAIL,
+                recipient_list=[recipient],
+            )
+            self.stdout.write(self.style.SUCCESS(f'Письмо успешно отправлено на {recipient}'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Ошибка отправки письма: {e}'))
---
+++ backend/users/management/commands/send_test_tg_message.py
@@ -0,0 +1,34 @@
+import requests
+from django.core.mail import send_mail
+from django.core.management.base import BaseCommand
+
+from kds_stroy.settings import DEFAULT_FROM_EMAIL, TG_BOT_TOKEN
+
+
+# python manage.py send_test_tg_message 22334455
+
+class Command(BaseCommand):
+    help = 'Отправить тестовое сообщение'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--to',
+            type=str,
+            help='ID получателя',
+            required=True,
+        )
+
+    def handle(self, *args, **options):
+        recipient = options['to']
+        base_url = f'https://api.telegram.org/bot{TG_BOT_TOKEN}'
+        text_api = f'{base_url}/sendMessage'
+
+        try:
+            requests.post(text_api, data={
+                'chat_id': recipient,
+                'text': "Тестовое сообщение со смайликом 🐥",
+                'parse_mode': 'Markdown'
+            })
+            self.stdout.write(self.style.SUCCESS(f'Сообщение успешно отправлено на {recipient}'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Ошибка отправки сообщения: {e}'))
